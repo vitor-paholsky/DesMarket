c73fb76c9e85c7263250c8a4af8f3b4d09418c29
vitor <vitor_paduarte@hotmail.com>
2023-08-13 21:39:11 -0300

    exc

--- BlazorWeb/wwwroot/js/code.js
+++
@@ -1,8 +0,0 @@
-function applyMaskToInput(inputElement) {
-    VMasker(inputElement).maskMoney({
-        precision: 2,
-        separator: ',',
-        delimiter: '.',
-        unit: 'R$'
-    });
-}
